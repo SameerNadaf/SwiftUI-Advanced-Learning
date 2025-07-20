33b64efbdb3f571061c8236880de039d33cb8800
Sameer Nadaf <nsameernadaf@gmail.com>
2025-07-20 13:05:22 +0530

    Initial Commit

---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Sameer  on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
