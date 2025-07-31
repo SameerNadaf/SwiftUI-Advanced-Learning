7c6ef726299c7f8faefb9e535a42e8e96b2a4557
Sameer Nadaf <nsameernadaf@gmail.com>
2025-07-31 20:12:16 +0530

    add new file

---
+++ SwiftUIAdvancedLearning/CustomOperators.swift
@@ -0,0 +1,18 @@
+//
+//  CustomOperators.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Sameer  on 31/07/25.
+//
+
+import SwiftUI
+
+struct CustomOperators: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomOperators()
+}
